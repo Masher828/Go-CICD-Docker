9d586de8278f019f7b48459e687ca7b5791ae8ba
Manish Sharma <manish.cse828@gmail.com>
2023-06-13 17:43:08 +0530

    add api

--- main.go
+++ main.go
@@ -26,6 +26,10 @@ func main() {
 		ctx.String(http.StatusOK, "Hola From Go updated")
 	})
 
+	app.GET("/si", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Yes From Go")
+	})
+
 	err := app.Run(":80")
 	if err != nil {
 		fmt.Println("Error while starting server", err)
