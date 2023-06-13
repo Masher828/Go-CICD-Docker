92a22ce2bf11fbb8807c0fbc622f7d8b37123002
Manish Sharma <manish.cse828@gmail.com>
2023-06-13 12:28:30 +0530

    add-server

--- main.go
+++ main.go
@@ -1,7 +1,25 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
 
-func main(){
-	fmt.Println("Hello from Golang")
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	app := gin.Default()
+
+	app.GET("/hello", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Hello From Go")
+	})
+
+	app.GET("/bye", func(ctx *gin.Context) {
+		ctx.String(http.StatusOK, "Bye From Go")
+	})
+
+	err := app.Run(":80")
+	if err != nil {
+		fmt.Println("Error while starting server", err)
+	}
 }
