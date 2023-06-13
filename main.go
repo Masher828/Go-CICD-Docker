6ab96d67ba711bae5b0e7dad7a9469b3037fa6d6
Manish Sharma <manish.cse828@gmail.com>
2023-06-13 17:29:35 +0530

    add updated message

--- main.go
+++ main.go
@@ -11,11 +11,11 @@ func main() {
 	app := gin.Default()
 
 	app.GET("/hello", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "Hello From Go")
+		ctx.String(http.StatusOK, "Hello From Go updated")
 	})
 
 	app.GET("/bye", func(ctx *gin.Context) {
-		ctx.String(http.StatusOK, "Bye From Go")
+		ctx.String(http.StatusOK, "Bye From Go updated")
 	})
 
 	err := app.Run(":80")
